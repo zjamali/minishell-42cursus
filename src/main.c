5a3b8d2bee3f74842ab6c10e5a8a0f944eeb0842
Mehdi Bari <mbari@e2r1p3.1337.ma>
2021-04-24 16:22:07 +0000

    sfds

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: zjamali <zjamali@student.42.fr>            +#+  +:+       +#+        */
+/*   By: mbari <mbari@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/11 15:07:04 by zjamali           #+#    #+#             */
-/*   Updated: 2021/04/24 15:11:20 by zjamali          ###   ########.fr       */
+/*   Updated: 2021/04/24 16:12:19 by mbari            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,6 +48,7 @@ int main(int ac,char **av,char **env)
 	int i = 0;
 	
 	readline = ft_init_readline(&termios);
+	head = NULL;
 	cmd = NULL;
 	init_env(&head, env);
 	while (i == 0)

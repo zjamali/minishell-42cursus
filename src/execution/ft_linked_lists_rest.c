78ad5dcfcc3dd403431958e0d7323e12568afffe
Mehdi Bari <mbari@e2r3p3.1337.ma>
2021-06-02 20:05:20 +0100

    complete norming all the files in executon

--- src/execution/ft_linked_lists_rest.c
+++ src/execution/ft_linked_lists_rest.c
@@ -1,98 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   list.c                                             :+:      :+:    :+:   */
+/*   ft_linked_lists_rest.c                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mbari <mbari@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/03/26 16:55:58 by mbari             #+#    #+#             */
-/*   Updated: 2021/05/31 18:01:57 by mbari            ###   ########.fr       */
+/*   Created: 2021/06/02 19:50:02 by mbari             #+#    #+#             */
+/*   Updated: 2021/06/02 20:02:48 by mbari            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../headers/execution.h"
-
-t_env	*ft_create_node(char *name, char *value)
-{
-	t_env	*new;
-
-	new = malloc(sizeof(t_env));
-	if (new)
-	{
-		new->name = ft_strdup(name);
-		new->value = ft_strdup(value);
-		new->next = NULL;
-	}
-	return (new);
-}
-
-void	ft_add_to_list(t_env **head, t_env *newnode)
-{
-	t_env	*list;
-
-	if (!head || !newnode)
-		return ;
-	if (*head)
-	{
-		list = *head;
-		while (list && list->next)
-			list = list->next;
-		list->next = newnode;
-	}
-	else
-		*head = newnode;
-}
-
-int	ft_delete_first_node(t_env **head, t_env	*temp, char *name)
-{
-	if (!(ft_strcmp(temp->name, name)))
-	{
-		*head = temp->next;
-		free(temp->name);
-		free(temp->value);
-		free(temp);
-		return (1);
-	}
-	return (0);
-}
-
-void	ft_delete_from_list(t_env **head, char *name)
-{
-	t_env	*prev;
-	t_env	*temp;
-
-	temp = *head;
-	if (*head == NULL)
-		return ;
-	if (ft_delete_first_node(head, temp, name))
-		return ;
-	while (temp != NULL && ft_strcmp(temp->name, name) != 0)
-	{
-		prev = temp;
-		temp = temp->next;
-	}
-	if (temp == NULL)
-		return ;
-	else
-	{
-		prev->next = temp->next;
-		free(temp->name);
-		free(temp->value);
-		free(temp);
-	}
-}
-
-t_env	*ft_search_in_list(t_env **head, char *name)
-{
-	t_env	*temp;
-
-	temp = *head;
-	if (temp == NULL)
-		return (NULL);
-	while (temp != NULL && ft_strcmp(temp->name, name) != 0)
-		temp = temp->next;
-	return (temp);
-}
 
 void	ft_replaceit(t_env **head, char *name, char *value)
 {
@@ -104,21 +22,6 @@ void	ft_replaceit(t_env **head, char *name, char *value)
 		free(temp->value);
 		temp->value = ft_strdup(value);
 	}
-}
-
-int	ft_count_list(t_env **head)
-{
-	int		i;
-	t_env	*temp;
-
-	i = 0;
-	temp = *head;
-	while (temp != NULL)
-	{
-		temp = temp->next;
-		i++;
-	}
-	return (i);
 }
 
 char	**ft_list_to_arr(t_env **head)
